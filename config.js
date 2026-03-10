dec3df39634a2b853983373f1fae9eb3e957a87c
SCoriolan <stalenes.coriolan@gmail.com>
2026-03-10 10:27:33 +0100

    Add database connection configuration using Mongoose in config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+//config.js
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/blog_api");
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
