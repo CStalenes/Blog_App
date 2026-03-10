7d9cf58d5b8cdb711965438b097d5914b8098f08
SCoriolan <stalenes.coriolan@gmail.com>
2026-03-10 11:37:08 +0100

    Add Express server setup and API endpoint for creating blogs

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
+const express = require("express");
 const connectDB = require("./config");
+const Blog = require("./blogSchema");
+
+const app = express();
+app.use(express.json());
+
 connectDB();
+
+app.post("/api/blogs", async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body);
+        res.json(blog);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
