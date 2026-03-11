da0d696b19ceedc55ecc918eb6d628a340f0cbe9
SCoriolan <stalenes.coriolan@gmail.com>
2026-03-11 01:44:59 +0100

    Add endpoint to retrieve user's blogs

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -11,4 +11,20 @@ router.post("/create-blog", async (req, res) => {
     }
 });
 
+router.get("/my-blogs", async (req, res) => {
+    try{
+        const myBlogs = await Blog.find();
+        res.status(200).json({ 
+            myBlogs,
+        });
+    } catch(err){
+        res.status(500).json({
+            message: "Error fetching blogs",
+            err,
+        })
+    }
+});
+
+//dump clef valeur pour transformer les dico en json
+
 module.exports = router;
