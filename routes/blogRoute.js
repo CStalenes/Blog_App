2e6274fcaf81bc292c3b3858c55831e462d2c1dd
SCoriolan <stalenes.coriolan@gmail.com>
2026-03-11 02:34:10 +0100

    Add endpoint to update existing blog posts with error handling

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -44,6 +44,39 @@ router.get("/my-blogs/:id", async (req, res) => {
     }
 });
 
+
+router.put("/update-blog/:id", async (req, res) =>{
+    try{
+       
+        const blogUpdated = await Blog.findOne({ _id: req.params.id });
+        if (!blogUpdated) {
+            return res.status(404).json({ message: "Blog not found" });
+        }
+
+        // Update the desired fields
+        // This line uses the ternary operator to update the blog title:
+        // If req.body.title is not undefined (i.e. if the request provides a new title),
+        // then blog.title will take this new value. Otherwise, blog.title keeps its current value.
+        
+        blogUpdated.title = req.body.title !== undefined ? req.body.title : blogUpdated.title;
+        blogUpdated.content = req.body.content !== undefined ? req.body.content : blogUpdated.content;
+        blogUpdated.author = req.body.author !== undefined ? req.body.author : blogUpdated.author;
+
+        // Save blog update
+        await blogUpdated.save();
+        res.status(200).json({ 
+            message: "Blog updated",
+            blogUpdated
+        });
+    } catch (err){
+        res.status(500).json({
+            message: "Blog not found",
+            err,
+        });
+    }
+
+});
+
 //dump clef valeur pour transformer les dico en json
 
 module.exports = router;
